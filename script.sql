cd28db8b5725b26e42ee0066c72bcd15ecef84e7
havillonf <havillonbfreitas@gmail.com>
2023-11-30 22:52:37 -0300

    fix syntax errors

--- script.sql
+++ script.sql
@@ -14,7 +14,7 @@ ON
 	PRIMARY
 	(
 		NAME = 'BDSpotPer',                         
-		FILENAME = 'C:\FBD_FINAL\BDSpotPer.mdf',   
+		FILENAME = 'C:\FBD-FINAL\BDSpotPer.mdf',   
 		SIZE = 5120KB,                              
 		FILEGROWTH = 1024KB                         
 	),
@@ -22,14 +22,14 @@ ON
 	FILEGROUP BDSpotPer_fg01
 	(
 		NAME = 'BDSpotPer_001',                     
-		FILENAME = 'C:\FBD_FINAL\BDSpotPer_001.ndf',
+		FILENAME = 'C:\FBD-FINAL\BDSpotPer_001.ndf',
 		SIZE = 1024KB,                              
 		FILEGROWTH = 30%                            
 	),
 
 	(
 		NAME = 'BDSpotPer_002',                     
-		FILENAME = 'C:\FBD_FINAL\BDSpotPer_002.ndf',
+		FILENAME = 'C:\FBD-FINAL\BDSpotPer_002.ndf',
 		SIZE = 1024KB,                              
 		MAXSIZE = 3072KB,                           
 		FILEGROWTH = 15%                            
@@ -38,7 +38,7 @@ ON
 	FILEGROUP BDSpotPer_fg02
 	(
 		NAME = 'BDSpotPer_003',                    
-		FILENAME = 'C:\FBD_FINAL\BDSpotPer_003.ndf',
+		FILENAME = 'C:\FBD-FINAL\BDSpotPer_003.ndf',
 		SIZE = 2048KB,                              
 		MAXSIZE = 5120KB,                          
 		FILEGROWTH = 30%                            
@@ -47,7 +47,7 @@ ON
 	LOG ON 
 	(
 		NAME = 'BDSpotPer_log',                     
-		FILENAME = 'C:\FBD_FINAL\BDSpotPer_log.ldf',
+		FILENAME = 'C:\FBD-FINAL\BDSpotPer_log.ldf',
 		SIZE = 1024KB,                              
 		FILEGROWTH = 10%                            
 	);
