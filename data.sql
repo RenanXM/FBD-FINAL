dddf278479ca8f3e5d2f91217c1c98d2d6fd523d
havillonf <havillonbfreitas@gmail.com>
2023-12-02 23:45:32 -0300

    added inserts

---
+++ data.sql
@@ -0,0 +1,77 @@
+INSERT INTO gravadora (cod, endereco, pagina, nome) VALUES (1, 'Rua A, 123', 'www.gravadora1.com.br', 'Gravadora 1');
+INSERT INTO gravadora (cod, endereco, pagina, nome) VALUES (2, 'Rua B, 456', 'www.gravadora2.com.br', 'Gravadora 2');
+INSERT INTO gravadora (cod, endereco, pagina, nome) VALUES (3, 'Rua C, 789', 'www.gravadora3.com.br', 'Gravadora 3');
+INSERT INTO gravadora (cod, endereco, pagina, nome) VALUES (4, 'Rua D, 123', 'www.gravadora4.com.br', 'Gravadora 4');
+INSERT INTO gravadora (cod, endereco, pagina, nome) VALUES (5, 'Rua E, 456', 'www.gravadora5.com.br', 'Gravadora 5');
+
+INSERT INTO album (cod, cod_grav, nome, descr, tipo_compra, pr_compra, dt_compra, dt_gravacao) VALUES (1, 1, 'Album 1', 'Descrição do Album 1', 'fisica', 10.00, '2023-12-01', '2023-11-01');
+INSERT INTO album (cod, cod_grav, nome, descr, tipo_compra, pr_compra, dt_compra, dt_gravacao) VALUES (2, 2, 'Album 2', 'Descrição do Album 2', 'download', 5.00, '2023-12-02', '2023-11-02');
+INSERT INTO album (cod, cod_grav, nome, descr, tipo_compra, pr_compra, dt_compra, dt_gravacao) VALUES (3, 3, 'Album 3', 'Descrição do Album 3', 'fisica', 15.00, '2023-12-03', '2023-11-03');
+INSERT INTO album (cod, cod_grav, nome, descr, tipo_compra, pr_compra, dt_compra, dt_gravacao) VALUES (4, 1, 'Album 4', 'Descrição do Album 4', 'fisica', 20.00, '2023-12-04', '2023-11-04');
+INSERT INTO album (cod, cod_grav, nome, descr, tipo_compra, pr_compra, dt_compra, dt_gravacao) VALUES (5, 2, 'Album 5', 'Descrição do Album 5', 'download', 7.00, '2023-12-05', '2023-11-05');
+
+INSERT INTO telefone (numero, cod_grav) VALUES ('(11) 1234-5678', 1);
+INSERT INTO telefone (numero, cod_grav) VALUES ('(11) 2345-6789', 2);
+INSERT INTO telefone (numero, cod_grav) VALUES ('(11) 3456-7890', 3);
+INSERT INTO telefone (numero, cod_grav) VALUES ('(11) 4567-8901', 4);
+INSERT INTO telefone (numero, cod_grav) VALUES ('(11) 5678-9012', 5);
+
+INSERT INTO composicao (cod, nome, descr) VALUES (1, 'Composição 1', 'Descrição da Composição 1');
+INSERT INTO composicao (cod, nome, descr) VALUES (2, 'Composição 2', 'Descrição da Composição 2');
+INSERT INTO composicao (cod, nome, descr) VALUES (3, 'Composição 3', 'Descrição da Composição 3');
+INSERT INTO composicao (cod, nome, descr) VALUES (4, 'Composição 4', 'Descrição da Composição 4');
+INSERT INTO composicao (cod, nome, descr) VALUES (5, 'Composição 5', 'Descrição da Composição 5');
+
+INSERT INTO faixa (numero, cod_album, descr, tempo, tipo_composicao, tipo_grav) VALUES (1, 1, 'Descrição da Faixa 1', '00:03:30', 1, 'ADD');
+INSERT INTO faixa (numero, cod_album, descr, tempo, tipo_composicao, tipo_grav) VALUES (2, 1, 'Descrição da Faixa 2', '00:04:00', 2, 'DDD');
+INSERT INTO faixa (numero, cod_album, descr, tempo, tipo_composicao, tipo_grav) VALUES (3, 2, 'Descrição da Faixa 3', '00:03:45', 3, 'ADD');
+INSERT INTO faixa (numero, cod_album, descr, tempo, tipo_composicao, tipo_grav) VALUES (4, 2, 'Descrição da Faixa 4', '00:03:30', 4, 'ADD');
+INSERT INTO faixa (numero, cod_album, descr, tempo, tipo_composicao, tipo_grav) VALUES (5, 2, 'Descrição da Faixa 5', '00:04:00', 5, 'DDD');
+
+INSERT INTO playlist (cod, nome, dt_criacao, dt_ult_reprod, num_reprod, tempo) VALUES (1, 'Playlist 1', '2023-12-01', '2023-12-02', 10, '00:20:00');
+INSERT INTO playlist (cod, nome, dt_criacao, dt_ult_reprod, num_reprod, tempo) VALUES (2, 'Playlist 2', '2023-12-02', '2023-12-03', 15, '00:30:00');
+INSERT INTO playlist (cod, nome, dt_criacao, dt_ult_reprod, num_reprod, tempo) VALUES (3, 'Playlist 3', '2023-12-03', '2023-12-04', 20, '00:40:00');
+INSERT INTO playlist (cod, nome, dt_criacao, dt_ult_reprod, num_reprod, tempo) VALUES (4, 'Playlist 4', '2023-12-04', '2023-12-05', 25, '01:00:00');
+INSERT INTO playlist (cod, nome, dt_criacao, dt_ult_reprod, num_reprod, tempo) VALUES (5, 'Playlist 5', '2023-12-05', '2023-12-06', 30, '01:30:00');
+
+INSERT INTO faixa_playlist (numero_faixa, cod_album, cod_playlist) VALUES (1, 1, 1);
+INSERT INTO faixa_playlist (numero_faixa, cod_album, cod_playlist) VALUES (2, 1, 2);
+INSERT INTO faixa_playlist (numero_faixa, cod_album, cod_playlist) VALUES (3, 2, 3);
+INSERT INTO faixa_playlist (numero_faixa, cod_album, cod_playlist) VALUES (4, 2, 4);
+INSERT INTO faixa_playlist (numero_faixa, cod_album, cod_playlist) VALUES (5, 2, 5);
+
+INSERT INTO interprete (cod, nome, tipo) VALUES (1, 'Interprete 1', 'Tipo 1');
+INSERT INTO interprete (cod, nome, tipo) VALUES (2, 'Interprete 2', 'Tipo 2');
+INSERT INTO interprete (cod, nome, tipo) VALUES (3, 'Interprete 3', 'Tipo 3');
+INSERT INTO interprete (cod, nome, tipo) VALUES (4, 'Interprete 4', 'Tipo 4');
+INSERT INTO interprete (cod, nome, tipo) VALUES (5, 'Interprete 5', 'Tipo 5');
+
+INSERT INTO faixa_interprete (numero_faixa, cod_album, cod_interp) VALUES (1, 1, 1);
+INSERT INTO faixa_interprete (numero_faixa, cod_album, cod_interp) VALUES (2, 1, 2);
+INSERT INTO faixa_interprete (numero_faixa, cod_album, cod_interp) VALUES (3, 2, 3);
+INSERT INTO faixa_interprete (numero_faixa, cod_album, cod_interp) VALUES (4, 2, 4);
+INSERT INTO faixa_interprete (numero_faixa, cod_album, cod_interp) VALUES (5, 2, 5);
+
+INSERT INTO compositor (cod, nome, local_nasc, dt_nasc, dt_morte) VALUES (1, 'Compositor 1', 'Local de Nascimento 1', '2000-01-01', NULL);
+INSERT INTO compositor (cod, nome, local_nasc, dt_nasc, dt_morte) VALUES (2, 'Compositor 2', 'Local de Nascimento 2', '2000-01-02', '2023-12-01');
+INSERT INTO compositor (cod, nome, local_nasc, dt_nasc, dt_morte) VALUES (3, 'Compositor 3', 'Local de Nascimento 3', '2000-01-03', NULL);
+INSERT INTO compositor (cod, nome, local_nasc, dt_nasc, dt_morte) VALUES (4, 'Compositor 4', 'Local de Nascimento 4', '2000-01-04', NULL);
+INSERT INTO compositor (cod, nome, local_nasc, dt_nasc, dt_morte) VALUES (5, 'Compositor 5', 'Local de Nascimento 5', '2000-01-05', '2023-12-02');
+
+INSERT INTO faixa_compositor (numero_faixa, cod_album, cod_composit) VALUES (1, 1, 1);
+INSERT INTO faixa_compositor (numero_faixa, cod_album, cod_composit) VALUES (2, 1, 2);
+INSERT INTO faixa_compositor (numero_faixa, cod_album, cod_composit) VALUES (3, 2, 3);
+INSERT INTO faixa_compositor (numero_faixa, cod_album, cod_composit) VALUES (4, 2, 4);
+INSERT INTO faixa_compositor (numero_faixa, cod_album, cod_composit) VALUES (5, 2, 5);
+
+INSERT INTO periodo_musc (cod, descr, intervalo) VALUES (1, 'Período Musical 1', 'Intervalo 1');
+INSERT INTO periodo_musc (cod, descr, intervalo) VALUES (2, 'Período Musical 2', 'Intervalo 2');
+INSERT INTO periodo_musc (cod, descr, intervalo) VALUES (3, 'Período Musical 3', 'Intervalo 3');
+INSERT INTO periodo_musc (cod, descr, intervalo) VALUES (4, 'Período Musical 4', 'Intervalo 4');
+INSERT INTO periodo_musc (cod, descr, intervalo) VALUES (5, 'Período Musical 5', 'Intervalo 5');
+
+INSERT INTO compositor_periodo_music (cod_composit, cod_periodo) VALUES (1, 1);
+INSERT INTO compositor_periodo_music (cod_composit, cod_periodo) VALUES (2, 2);
+INSERT INTO compositor_periodo_music (cod_composit, cod_periodo) VALUES (3, 3);
+INSERT INTO compositor_periodo_music (cod_composit, cod_periodo) VALUES (4, 4);
+INSERT INTO compositor_periodo_music (cod_composit, cod_periodo) VALUES (5, 5);
--- tables_creation.sql
+++ tables_creation.sql
@@ -1,134 +1,166 @@
 USE BDSpotPer;
 
-CREATE TABLE gravadora(
-	cod SMALLINT NOT NULL,
-	endereco VARCHAR(100) NOT NULL,
-	pagina VARCHAR(100) NOT NULL,
-	nome VARCHAR(20) NOT NULL,
-	CONSTRAINT PK_GRAV PRIMARY KEY(cod)
+CREATE TABLE Album(
+	codigo INTEGER NOT NULL,
+    descricao VARCHAR (100) NOT NULL, 
+	fk_codigo_gravadora INTEGER  NOT NULL,
+	nome VARCHAR (50) NOT NULL,
+	data_compra DATE NOT NULL,
+	data_gravacao DATE NOT NULL,
+    tipo_compra VARCHAR (20) NOT NULL,
+    fk_codigo_meio_fisico INTEGER NOT NULL,
+    preco_compra DEC(5,2) NOT NULL,
+
+	CONSTRAINT PK_ALBUM PRIMARY KEY (codigo),
+    CONSTRAINT FK_MEIO_FISICO FOREIGN KEY(fk_codigo_meio_fisico) REFERENCES Meio_Fisico,
+    CONSTRAINT DATA_GRAVACAO CHECK (data_gravacao > '2000-01-01'),
+	CONSTRAINT FK_GRAVADORA_A FOREIGN KEY (fk_codigo_gravadora) REFERENCES Gravadora,
+	CONSTRAINT TIPO_COMPRA CHECK (tipo_compra LIKE '__sica' OR tipo_compra LIKE '_ownload')
 
 ) ON BDSpotPer_fg01;
 
-CREATE TABLE album(
-	cod SMALLINT NOT NULL,
-	cod_grav SMALLINT NOT NULL,
-	nome VARCHAR(50) NOT NULL,
-	descr VARCHAR(100) NOT NULL, 
-	tipo_compra VARCHAR(20) NOT NULL,
-	pr_compra DEC(5,2) NOT NULL,
-	dt_compra DATE NOT NULL,
-	dt_gravacao DATE NOT NULL,
-	
-	CONSTRAINT PK_ALBM PRIMARY KEY (cod),
-	CONSTRAINT FK_GRAV_ALBM FOREIGN KEY (cod_grav) REFERENCES gravadora,
-	CONSTRAINT TIPO_DA_COMPRA CHECK (tipo_compra LIKE 'fisica' or tipo_compra LIKE 'download'),
-	CONSTRAINT DATA_COMPRA_SEC_XXI CHECK (dt_compra > '01/01/2000')
+CREATE TABLE Meio_Fisico(
+    codigo INTEGER NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
+
+    CONSTRAINT PK_MEIO_FISICO PRIMARY KEY(codigo)
 
 ) ON BDSpotPer_fg01;
 
-CREATE TABLE telefone(
-	numero VARCHAR(13) NOT NULL,
-	cod_grav SMALLINT NOT NULL,
-	CONSTRAINT PK_TEL PRIMARY KEY (numero),
-	CONSTRAINT FK_GRAV_TEL FOREIGN KEY (cod_grav) REFERENCES gravadora
+CREATE TABLE Faixa(
+	codigo_faixa INTEGER  NOT NULL,
+	numero INTEGER  NOT NULL,
+	fk_codigo_album INTEGER  NOT NULL,
+	descricao VARCHAR (100) NOT NULL, 
+	tempo time(0) NOT NULL,
+	tipo_composicao INTEGER  NOT NULL,
+	tipo_gravacao VARCHAR (3) NOT NULL
 
-) ON BDSpotPer_fg01;
-
-CREATE TABLE composicao(
-	cod SMALLINT NOT NULL,
-	nome VARCHAR(20) NOT NULL,
-	descr VARCHAR(100) NOT NULL,
-
-	CONSTRAINT PK_COMPOSICAO PRIMARY KEY (cod)
-
-) ON BDSpotPer_fg01;
-
-CREATE TABLE faixa(
-	numero SMALLINT NOT NULL,
-	cod_album SMALLINT NOT NULL,
-	descr VARCHAR(100) NOT NULL, 
-	tempo TIME(0) NOT NULL,
-	tipo_composicao SMALLINT NOT NULL,
-	tipo_grav VARCHAR(3) NOT NULL
-
-	CONSTRAINT FK_ALBUM_FAIXA FOREIGN KEY (cod_album) REFERENCES album ON DELETE CASCADE,
-	CONSTRAINT ADD_OU_DDD CHECK (tipo_grav LIKE 'ADD' or tipo_grav LIKE 'DDD')
+	CONSTRAINT PK_FAIXA PRIMARY KEY (codigo_faixa)
+	CONSTRAINT FK_ALBUM_F FOREIGN KEY (fk_codigo_album) REFERENCES Album ON DELETE CASCADE
+	CONSTRAINT ADD_OU_DDD CHECK (tipo_gravacao LIKE 'ADD' OR tipo_gravacao LIKE 'DDD')
 	
 ) ON BDSpotPer_fg02;
 
-CREATE TABLE playlist(
-	cod SMALLINT NOT NULL,
-	nome VARCHAR(20) NOT NULL,
-	dt_criacao DATE NOT NULL,
-	dt_ult_reprod DATE NOT NULL,
-	num_reprod SMALLINT NOT NULL,
-	tempo TIME(0) NOT NULL,
+CREATE TABLE Meio_Fisico_Faixa(
+    codigo INTEGER NOT NULL,
+    numero_faixa INTEGER NOT NULL,
+    fk_codigo_meio_fisico INTEGER NOT NULL,
+    fk_codigo_faixa INTEGER NOT NULL,
 
-	CONSTRAINT PK_PLAYLIST PRIMARY KEY (cod)
+    CONSTRAINT PK_MEIO_FISICO_FAIXA PRIMARY KEY(codigo),
+    CONSTRAINT FK_MEIO_FISICO_MFA FOREIGN KEY (fk_codigo_meio_fisico) REFERENCES Meio_Fisico,
+    CONSTRAINT FK_FAIXA_MFA FOREIGN KEY (fk_codigo_faixa) REFERENCES Faixa
+
+)ON BDSpotPer_fg01;
+
+CREATE TABLE Gravadora(
+	codigo INTEGER  NOT NULL,
+	endereco VARCHAR (100) NOT NULL,
+	pagina VARCHAR (100) NOT NULL,
+	nome VARCHAR (20) NOT NULL,
+	CONSTRAINT PK_GRAVADORA PRIMARY KEY (codigo)
+
+) ON BDSpotPer_fg01;
+
+CREATE TABLE Telefone(
+	numero VARCHAR (13) NOT NULL,
+	codigo_gravadora INTEGER  NOT NULL,
+	CONSTRAINT PK_TELEFONE PRIMARY KEY (numero),
+	CONSTRAINT FK_GRAVADORA_T FOREIGN KEY (codigo_gravadora) REFERENCES Gravadora
+
+) ON BDSpotPer_fg01;
+
+CREATE TABLE Composicao(
+	codigo INTEGER  NOT NULL,
+	nome VARCHAR (20) NOT NULL,
+	descricao VARCHAR (100) NOT NULL, -- Sinfonia, opera, sonata, concerto
+
+	CONSTRAINT PK_COMPOSICAO PRIMARY KEY (codigo)
+
+) ON BDSpotPer_fg01;
+
+CREATE TABLE Playlist(
+	codigo INTEGER  NOT NULL,
+	nome VARCHAR (20) NOT NULL,
+	data_criacao DATE NOT NULL,
+	data_ultima_reproducao DATE NOT NULL,
+	numero_reproducao INTEGER  NOT NULL,
+	tempo time(0) NOT NULL,
+
+	CONSTRAINT PK_PLAYLIST PRIMARY KEY (codigo)
 
 ) ON BDSpotPer_fg02;
 
-CREATE TABLE faixa_playlist(
-	numero_faixa SMALLINT NOT NULL,
-	cod_album SMALLINT NOT NULL,
-	cod_playlist SMALLINT NOT NULL,
+CREATE TABLE Faixa_Playlist(
+	numero_faixa INTEGER  NOT NULL,
+	codigo_album INTEGER  NOT NULL,
+	fk_codigo_playlist INTEGER  NOT NULL,
+	fk_codigo_faixa INTEGER  NOT NULL,
 
-	CONSTRAINT FK_PLAYLIST_FP FOREIGN KEY (cod_playlist) REFERENCES playlist
+	CONSTRAINT FK_PLAYLIST_FP FOREIGN KEY (fk_codigo_playlist) REFERENCES Playlist,
+	CONSTRAINT FK_FAIXA_FP FOREIGN KEY (fk_codigo_faixa) REFERENCES Faixa
 
 ) ON BDSpotPer_fg02;
 
-CREATE TABLE interprete(
-	cod SMALLINT NOT NULL,	
-	nome VARCHAR(20) NOT NULL,
-	tipo VARCHAR(20) NOT NULL,
+CREATE TABLE Interprete(
+	codigo INTEGER NOT NULL,	
+	nome VARCHAR (20) NOT NULL,
+	tipo VARCHAR (20) NOT NULL, --  orquestra, trio, quarteto, ensemble, soprano, tenor..
 
-	CONSTRAINT PK_INTERP PRIMARY KEY (cod)
-
-) ON BDSpotPer_fg01;
-
-CREATE TABLE faixa_interprete(
-	numero_faixa SMALLINT NOT NULL,
-	cod_album SMALLINT NOT NULL,
-	cod_interp SMALLINT NOT NULL,
-
-	CONSTRAINT FK_INTERP_I FOREIGN KEY (cod_interp) REFERENCES interprete
+	CONSTRAINT PK_INTERPRETE PRIMARY KEY (codigo)
 
 ) ON BDSpotPer_fg01;
 
-CREATE TABLE compositor(
-	cod SMALLINT NOT NULL,	
-	nome VARCHAR(20) NOT NULL,
-	local_nasc VARCHAR(100) NOT NULL,
-	dt_nasc DATE NOT NULL, 
-	dt_morte DATE,
+CREATE TABLE Faixa_Interprete(
+	numero_faixa INTEGER NOT NULL,
+	codigo_album INTEGER NOT NULL,
+	fk_codigo_interprete INTEGER NOT NULL,
+	fk_codigo_faixa INTEGER NOT NULL,
 
-	CONSTRAINT PK_COMPOSITOR PRIMARY KEY (cod)
-
-) ON BDSpotPer_fg01;
-
-CREATE TABLE faixa_compositor(
-	numero_faixa SMALLINT NOT NULL,
-	cod_album SMALLINT NOT NULL,
-	cod_composit SMALLINT NOT NULL,
-
-	CONSTRAINT FK_COMPOSIT_C FOREIGN KEY (cod_composit) REFERENCES compositor
+	CONSTRAINT FK_INTERPRETE_FI FOREIGN KEY (fk_codigo_interprete) REFERENCES Interprete,
+	CONSTRAINT FK_FAIXA_FI FOREIGN KEY (fk_codigo_faixa) REFERENCES Faixa
 
 ) ON BDSpotPer_fg01;
 
-CREATE TABLE periodo_musc(
-	cod SMALLINT NOT NULL,
-	descr VARCHAR(100) NOT NULL,
-	intervalo VARCHAR(30) NOT NULL, 
+CREATE TABLE Compositor(
+	codigo INTEGER NOT NULL,	
+	nome VARCHAR (20) NOT NULL,
+	local_nascimento VARCHAR (100) NOT NULL,
+	data_nascimento DATE NOT NULL, 
+	data_morte DATE,
 
-	CONSTRAINT PK_PERIODO PRIMARY KEY (cod)
+	CONSTRAINT PK_COMPOSITOR PRIMARY KEY (codigo)
 
 ) ON BDSpotPer_fg01;
 
-CREATE TABLE compositor_periodo_music(
-	cod_composit SMALLINT NOT NULL,
-	cod_periodo SMALLINT NOT NULL,
+CREATE TABLE Faixa_Compositor(
+	numero_faixa INTEGER NOT NULL,
+	codigo_album INTEGER NOT NULL,
+	fk_codigo_compositor INTEGER NOT NULL,
+	fk_codigo_faixa INTEGER NOT NULL,
 
-	CONSTRAINT FK_COMPOSIT_CPM FOREIGN KEY (cod_composit) REFERENCES compositor,
-	CONSTRAINT FK_PERIODO_CPM FOREIGN KEY (cod_periodo) REFERENCES periodo_musc
+	CONSTRAINT FK_COMPOSITOR_FC FOREIGN KEY (fk_codigo_compositor) REFERENCES Compositor,
+	CONSTRAINT FK_FAIXA_FC FOREIGN KEY (fk_codigo_faixa) REFERENCES Faixa
+
+) ON BDSpotPer_fg01;
+
+CREATE TABLE Periodo_Musical(
+	codigo INTEGER NOT NULL,
+	descricao VARCHAR (100) NOT NULL, --barroco, classico, romantico, etc..
+	intervalo VARCHAR (30) NOT NULL, 
+
+	CONSTRAINT PK_PERIODO_MUSICAL PRIMARY KEY (codigo)
+
+) ON BDSpotPer_fg01;
+
+CREATE TABLE Compositor_Periodo_Musical(
+	codigo_compositor INTEGER NOT NULL,
+	codigo_periodo INTEGER NOT NULL,
+
+	CONSTRAINT FK_COMPOSITOR_CPM FOREIGN KEY (codigo_compositor) REFERENCES Compositor,
+	CONSTRAINT FK_PERIODO_CPM FOREIGN KEY (codigo_periodo) REFERENCES Periodo_Musical
 
 ) ON BDSpotPer_fg01;
