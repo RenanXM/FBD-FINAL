87a4624def20ec2dd582b2a8f6081150e0434726
havillonf <havillonbfreitas@gmail.com>
2023-12-04 00:09:22 -0300

    fixed database credentials

--- spotper.py
+++ spotper.py
@@ -9,9 +9,9 @@ import os
 # Function to connect to the database
 
 # Crie variáveis para suas credenciais de conexão.
-SERVER = 'DESKTOP-R31K86D'
+SERVER = 'DESKTOP-UCMJFKJ'
 DATABASE = 'BDSpotPer'
-USERNAME = 'DESKTOP-R31K86D\Havillon'
+USERNAME = 'DESKTOP-UCMJFKJ\havil'
 PASSWORD = ''
 
 # Crie uma variável de cadeia de conexão usando interpolação de cadeia de caracteres.
