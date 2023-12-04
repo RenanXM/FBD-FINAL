cd1192f2e3ed1397b9c8ed26bdaab927bcefabef
havillonf <havillonbfreitas@gmail.com>
2023-12-04 04:03:47 -0300

    Q6 function reviewed

--- functions_creation.sql
+++ functions_creation.sql
@@ -1,12 +1,20 @@
+-- 6) Defina uma função que tem como parâmetro de entrada o nome (ou parte do) 
+-- nome do compositor e o parâmetro de saída todos os álbuns com obras 
+-- compostas pelo compositor
 create function album_compositor (@nome varchar)
 	returns @tabela_obras table
-	(albuns_compositor nvarchar(30))
+	(albuns_compositor varchar(30))
 as
 begin
 	insert into @tabela_obras
-	select distinct a.nome from compositor c, faixa_compositor fc, faixa f, album a 
-		where c.nome like ('%'+@nome+'%') and c.cod=fc.cod_composit
-			  and fc.numero_faixa=f.numero and f.cod_album=a.cod
+	select distinct a.nome 
+	from compositor c
+	inner join faixa_compositor fc on c.cod = fc.cod_comp
+	inner join faixa f on fc.cod_faixa = f.cod
+	inner join meio_fisico_faixa mfa on f.cod = mfa.cod_faixa
+	inner join meio_fisico mf ON mfa.cod_meio_fisico = mf.cod
+	inner join album a ON mf.cod_album = a.cod
+	where c.nome like ('%'+@nome+'%')
 	return			
 end
 
